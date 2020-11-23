0ed8b572a4d6f33d7caf43faa1b79edbcbeef184
Junnior Jamil Hurtado Cachay <JuniorJamil@users.noreply.github.com>
2020-11-23 12:26:52 -0500

    Create Testing

---
+++ ILanguage.API/ILanguage.API.Test/SubscriptionServiceTest.cs
@@ -0,0 +1,65 @@
+﻿using ILanguage.API.Domain.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using ILanguage.API.Domain.Models;
+using ILanguage.API.Services;
+using ILanguage.API.Domain.Services.Communication;
+
+namespace ILanguage.API.Test
+{
+    public class SubscriptionServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public async Task ListAsyncWhenNoSubscriptionsReturnsEmptyCollection()
+        {
+            var mockSubscriptionRepository = GetDefaultISubscriptionRepositoryInstance();
+            mockSubscriptionRepository.Setup(r => r.ListAsync())
+                .ReturnsAsync(new List<Subscription>());
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var service = new SubscriptionService(
+                mockSubscriptionRepository.Object,
+                mockUnitOfWork.Object);
+
+            // Act
+            List<Subscription> result = (List<Subscription>)await service.ListAsync();
+            int subscriptionsCount = result.Count;
+
+            // Assert
+            subscriptionsCount.Should().Equals(0);
+        }
+
+        [Test]
+        public async Task GetByIdAsyncWhenInvalidIdReturnsSubscriptionNotFoundResponse()
+        {
+            // Arrange
+            var mockSubscriptionRepository = GetDefaultISubscriptionRepositoryInstance();
+            var subscriptionId = 1;
+            mockSubscriptionRepository.Setup(r => r.FindById(subscriptionId))
+                .Returns(Task.FromResult<Subscription>(null));
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var service = new SubscriptionService(mockSubscriptionRepository.Object, mockUnitOfWork.Object);
+            // Act
+            SubscriptionResponse result = await service.GetByIdAsync(subscriptionId);
+            var message = result.Message;
+            // Assert
+            message.Should().Be("Subscription not found");
+        }
+
+        private Mock<ISubscriptionRepository> GetDefaultISubscriptionRepositoryInstance()
+        {
+            return new Mock<ISubscriptionRepository>();
+        }
+
+        private Mock<IUnitOfWork> GetDefaultIUnitOfWorkInstance()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+    }
+}
---
+++ ILanguage.API/ILanguage.API.Test/UserServiceTest.cs
@@ -0,0 +1,65 @@
+﻿using ILanguage.API.Domain.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using ILanguage.API.Domain.Models;
+using ILanguage.API.Services;
+using ILanguage.API.Domain.Services.Communication;
+
+namespace EasyNutrition.API.Test
+{
+    public class UserServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public async Task ListAsyncWhenNoUsersReturnsEmptyCollection()
+        {
+            var mockUserRepository = GetDefaultIUserRepositoryInstance();
+            mockUserRepository.Setup(r => r.ListAsync())
+                .ReturnsAsync(new List<User>());
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var service = new UserService(
+                mockUserRepository.Object,
+                mockUnitOfWork.Object);
+
+            // Act
+            List<User> result = (List<User>)await service.ListAsync();
+            int usersCount = result.Count;
+
+            // Assert
+            usersCount.Should().Equals(0);
+        }
+
+        [Test]
+        public async Task GetByIdAsyncWhenInvalidIdReturnsUserNotFoundResponse()
+        {
+            // Arrange
+            var mockUserRepository = GetDefaultIUserRepositoryInstance();
+            var userId = 1;
+            mockUserRepository.Setup(r => r.FindById(userId))
+                .Returns(Task.FromResult<User>(null));
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var service = new UserService(mockUserRepository.Object, mockUnitOfWork.Object);
+            // Act
+            UserResponse result = await service.GetByIdAsync(userId);
+            var message = result.Message;
+            // Assert
+            message.Should().Be("User not found");
+        }
+
+        private Mock<IUserRepository> GetDefaultIUserRepositoryInstance()
+        {
+            return new Mock<IUserRepository>();
+        }
+
+        private Mock<IUnitOfWork> GetDefaultIUnitOfWorkInstance()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+    }
+}
